0d0031657783ee91a412a12c672842fb341c5158
Jhonnatthan Alves <jaalvest@outlook.com>
2025-04-21 16:04:09 -0300

    Initial commit

---
+++ NEThook/Presentation/WebHook/Routes/StsUserEventRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace NEThook.Presentation.WebHook.Routes;
+
+public static class StsUserEventRoutes
+{
+    public static WebApplication RegisterEvent(this WebApplication app)
+    {
+        var group = app.MapGroup("/sts/user/event");
+
+        // mapeia o endpoint POST /sts/user/event/register
+        group.MapPost("/register", () => Results.Ok());
+
+        return app;
+    }
+}
